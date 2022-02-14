e14354c5d727a74eb31bdfaf42d11efb05e9f1fb
Tomasz Walecki <tomek_w@outlook.com>
2022-02-14 20:28:21 +0100

    Add SettingsService

---
+++ AoE2DELobbyNotifications/SettingsService.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using Windows.Storage;
+
+namespace AoE2DELobbyNotifications
+{
+    internal class SettingsService
+    {
+        private ApplicationDataContainer _localSettings;
+
+        public SettingsService()
+        {
+            _localSettings = ApplicationData.Current.LocalSettings;
+        }
+
+        public T Get<T>(string key, T defaultValue)
+        {
+            if(_localSettings.Values.TryGetValue(key, out object serialized))
+            {
+                var data = JsonSerializer.Deserialize<T>(serialized as string);
+                return data;
+            }
+            else
+            {
+                Save(key, defaultValue);
+                return defaultValue;
+            }
+        }
+
+        public void Save<T>(string key, T data)
+        {
+            var serialized = JsonSerializer.Serialize(data);
+            _localSettings.Values[key]=serialized;
+        }
+    }
+}
