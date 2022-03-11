a8742f391bd41eabae89009c7890c4fb1d85faf2
Tomasz Walecki <tomek_w@outlook.com>
2022-03-11 15:31:02 +0100

    Return results from aoe2net, add logger, cache

---
+++ AoE2DELobbyBrowser.WebApi/ApiCache.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+class ApiCache
+{
+    private static readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+    private readonly IMemoryCache _memoryCache;
+    private const string cacheKey = "cachedLobbiesKey";
+    private readonly TimeSpan cacheExpiration;
+
+    public ApiCache(IMemoryCache memoryCache, IConfiguration configuration)
+    {
+        _memoryCache = memoryCache;
+        var exp = configuration.GetValue<int>("CacheExpiration");
+        cacheExpiration = TimeSpan.FromSeconds(exp);
+    }
+
+    public async Task<string> GetOrCreateAsync(Func<Task<String>> func)
+    {
+        if (!_memoryCache.TryGetValue<string>(cacheKey, out string data))
+        {
+            await semaphoreSlim.WaitAsync();
+            try
+            {
+                if (!_memoryCache.TryGetValue<string>(cacheKey, out data))
+                {
+                    data = await func();
+                    _memoryCache.Set(cacheKey, data, cacheExpiration);
+                }
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
+        }
+        return data;
+    }
+}
--- AoE2DELobbyBrowser.WebApi/Program.cs
+++ AoE2DELobbyBrowser.WebApi/Program.cs
@@ -1,6 +1,50 @@
+using Serilog;
+using System.Net.Mime;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
+builder.Services.AddMemoryCache();
+builder.Logging.ClearProviders();
+var logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+builder.Logging.AddSerilog(logger);
+builder.Services.AddSingleton<ApiCache>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => 
+{
+    var html = $"<a href='https://github.com/tmk907/AoE2DELobbyBrowser'>AoE2DELobbyBrowser</a>";
+    return Results.Content(html, contentType: MediaTypeNames.Text.Html);
+});
 
+app.MapGet("/api", () =>
+{
+    var html = $"This API is used by <a href='https://github.com/tmk907/AoE2DELobbyBrowser'>AoE2DELobbyBrowser</a>";
+    return Results.Content(html, contentType: MediaTypeNames.Text.Html);
+});
+
+app.MapGet("/api/lobbies", async (IHttpClientFactory httpClientFactory, ApiCache cache) =>
+{
+    var data = await cache.GetOrCreateAsync(() =>
+    {
+        try
+        {
+            var url = "https://aoe2.net/api/lobbies?game=aoe2de";
+            var httpClient = httpClientFactory.CreateClient();
+            return httpClient.GetStringAsync(url);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex.ToString());
+            return Task.FromResult("");
+        }
+    });
+
+    return Results.Content(data, contentType: MediaTypeNames.Application.Json);
+});
+
+app.Logger.LogInformation("The application started");
 app.Run();
