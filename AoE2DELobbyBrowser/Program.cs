90658dc867894b24698d5672874ef9a12308e19a
Tomasz Walecki <tomek_w@outlook.com>
2022-04-02 17:48:09 +0200

    Fix errors related to async Main

--- AoE2DELobbyBrowser/Program.cs
+++ AoE2DELobbyBrowser/Program.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using Microsoft.UI.Dispatching;
 using Microsoft.Windows.AppLifecycle;
+using System.Runtime.InteropServices;
 
 namespace AoE2DELobbyBrowser
 {
@@ -11,32 +12,24 @@ namespace AoE2DELobbyBrowser
         private const string appInstanceKey = "419CEECD-FDDD-41E2-BC3D-E203208D7974";
 
         [STAThread]
-        static async Task<int> Main(string[] args)
+        static void Main(string[] args)
         {
-            System.Diagnostics.Debug.WriteLine($"LOG: Program Main {string.Join(",", args)}");
-
             WinRT.ComWrappersSupport.InitializeComWrappers();
-            bool isRedirect = await DecideRedirection();
+            bool isRedirect = DecideRedirection();
             if (!isRedirect)
             {
-                System.Diagnostics.Debug.WriteLine($"LOG:Program not Redirect");
-                Microsoft.UI.Xaml.Application.Start((p) =>
-                {
+                Microsoft.UI.Xaml.Application.Start((p) => {
                     var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
                     SynchronizationContext.SetSynchronizationContext(context);
                     new App();
                 });
             }
-
-            return 0;
         }
 
-        private static async Task<bool> DecideRedirection()
+        private static bool DecideRedirection()
         {
             bool isRedirect = false;
-
             AppActivationArguments args = AppInstance.GetCurrent().GetActivatedEventArgs();
-            ExtendedActivationKind kind = args.Kind;
 
             try
             {
@@ -45,8 +38,7 @@ namespace AoE2DELobbyBrowser
                 if (!keyInstance.IsCurrent)
                 {
                     isRedirect = true;
-                    System.Diagnostics.Debug.WriteLine("LOG:RedirectActivationTo");
-                    await keyInstance.RedirectActivationToAsync(args);
+                    RedirectActivationTo(args, keyInstance);
                 }
             }
 
@@ -56,6 +48,38 @@ namespace AoE2DELobbyBrowser
             }
 
             return isRedirect;
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr CreateEvent(
+            IntPtr lpEventAttributes, bool bManualReset,
+            bool bInitialState, string lpName);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool SetEvent(IntPtr hEvent);
+
+        [DllImport("ole32.dll")]
+        private static extern uint CoWaitForMultipleObjects(
+            uint dwFlags, uint dwMilliseconds, ulong nHandles,
+            IntPtr[] pHandles, out uint dwIndex);
+
+        private static IntPtr redirectEventHandle = IntPtr.Zero;
+
+        // Do the redirection on another thread, and use a non-blocking
+        // wait method to wait for the redirection to complete.
+        public static void RedirectActivationTo(AppActivationArguments args, AppInstance keyInstance)
+        {
+            redirectEventHandle = CreateEvent(IntPtr.Zero, true, false, null);
+            Task.Run(() =>
+            {
+                keyInstance.RedirectActivationToAsync(args).AsTask().Wait();
+                SetEvent(redirectEventHandle);
+            });
+            uint CWMO_DEFAULT = 0;
+            uint INFINITE = 0xFFFFFFFF;
+            _ = CoWaitForMultipleObjects(
+               CWMO_DEFAULT, INFINITE, 1,
+               new IntPtr[] { redirectEventHandle }, out uint handleIndex);
         }
     }
 }
