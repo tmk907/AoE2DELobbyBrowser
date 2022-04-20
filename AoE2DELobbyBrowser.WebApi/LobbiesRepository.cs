aaf7c4773fc605c3d62ce35271b19466bdb17bdf
Tomasz Walecki <tomek_w@outlook.com>
2022-04-20 19:54:39 +0200

    Remove lobbies with same game name hosted by one player

--- AoE2DELobbyBrowser.WebApi/LobbiesRepository.cs
+++ AoE2DELobbyBrowser.WebApi/LobbiesRepository.cs
@@ -7,48 +7,70 @@ namespace AoE2DELobbyBrowser.WebApi
         private readonly ILogger<LobbiesRepository> _logger;
         private readonly ConcurrentDictionary<string, LobbyDto> _lobbies;
 
+        private static SemaphoreSlim semaphore;
+
         public LobbiesRepository(ILogger<LobbiesRepository> logger)
         {
             _logger = logger;
             _lobbies = new ConcurrentDictionary<string, LobbyDto>();
+            semaphore = new SemaphoreSlim(0, 1);
         }
 
         public void AddLobbies(IEnumerable<Aoe2netWebsocket.LobbyDto> newLobbies)
         {
-            _logger.LogDebug($"Add lobbies {newLobbies.Count()}");
-            foreach(var lobby in newLobbies.Select(x=>Create(x)))
+            semaphore.Wait();
+            try
             {
-                _lobbies.AddOrUpdate(lobby.LobbyId, lobby, (key, oldLobby) => lobby);
-            }
-            
-            var notActive = newLobbies.Where(x => !x.Active).Select(x => x.SteamLobbyId).ToList();
-            if (notActive.Count > 0)
-            {
-                _logger.LogDebug($"Not active lobbies {notActive.Count}");
-            }
-            foreach (var key in notActive)
-            {
-                _lobbies.Remove(key, out LobbyDto _);
-            }
-            
-            var full = _lobbies.Where(x => x.Value.NumSlots == x.Value.NumPlayers).Select(x => x.Key).ToList();
-            _logger.LogDebug($"Full lobbies {full.Count} ");
-            foreach (var key in full)
-            {
-                _lobbies.Remove(key, out LobbyDto _);
-            }
+                // remove lobbies where first player and game name is the same
+                var nameToLobbyId = _lobbies.ToDictionary(x => GetHostAndGameName(x.Value), x => x.Key);
+                foreach(var lobby in newLobbies)
+                {
+                    var name = GetHostAndGameName(Create(lobby));
+                    if (nameToLobbyId.TryGetValue(name, out var lobbyId))
+                    {
+                        _lobbies.Remove(lobbyId, out _);
+                    }
+                }
 
-            var numSlotsZero = _lobbies.Where(x => x.Value.NumSlots == 0).Select(x => x.Key).ToList();
-            if (numSlotsZero.Count > 0)
-            {
-                _logger.LogDebug($"numSlotsZero lobbies {numSlotsZero.Count}");
+                _logger.LogDebug($"Add lobbies {newLobbies.Count()}");
+                foreach (var lobby in newLobbies.Select(x => Create(x)))
+                {
+                    _lobbies.AddOrUpdate(lobby.LobbyId, lobby, (key, oldLobby) => lobby);
+                }
+
+                var notActive = newLobbies.Where(x => !x.Active).Select(x => x.SteamLobbyId).ToList();
+                if (notActive.Count > 0)
+                {
+                    _logger.LogDebug($"Not active lobbies {notActive.Count}");
+                }
+                foreach (var key in notActive)
+                {
+                    _lobbies.Remove(key, out LobbyDto _);
+                }
+
+                var full = _lobbies.Where(x => x.Value.NumSlots == x.Value.NumPlayers).Select(x => x.Key).ToList();
+                _logger.LogDebug($"Full lobbies {full.Count} ");
+                foreach (var key in full)
+                {
+                    _lobbies.Remove(key, out LobbyDto _);
+                }
+
+                var numSlotsZero = _lobbies.Where(x => x.Value.NumSlots == 0).Select(x => x.Key).ToList();
+                if (numSlotsZero.Count > 0)
+                {
+                    _logger.LogDebug($"numSlotsZero lobbies {numSlotsZero.Count}");
+                }
+                foreach (var key in numSlotsZero)
+                {
+                    _lobbies.Remove(key, out LobbyDto _);
+                }
+
+                _logger.LogInformation($"Total lobbies {_lobbies.Count}");
             }
-            foreach (var key in numSlotsZero)
+            finally
             {
-                _lobbies.Remove(key, out LobbyDto _);
+                semaphore.Release();
             }
-            
-            _logger.LogInformation($"Total lobbies {_lobbies.Count}");
         }
 
         public IEnumerable<LobbyDto> GetLobbies()
@@ -78,6 +100,11 @@ namespace AoE2DELobbyBrowser.WebApi
                 Players = dto.Players.Select(x => Create(x)).ToList(),
                 Speed = dto.Speed
             };
+        }
+
+        private string GetHostAndGameName(LobbyDto lobby)
+        {
+            return $"{lobby.Players.FirstOrDefault()?.Name ?? ""}~{lobby.Name}";
         }
 
         private PlayerDto Create(Aoe2netWebsocket.PlayerDto dto)
