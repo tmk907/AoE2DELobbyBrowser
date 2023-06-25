70e7caad71f4fa7ea50bfa97bbfeaab13b5009a4
Tomasz Walecki <tomek_w@outlook.com>
2023-06-25 19:58:36 +0200

    Fix decoding options

--- AoE2DELobbyBrowser.WebApi/Reliclink/OptionsDecoder.cs
+++ AoE2DELobbyBrowser.WebApi/Reliclink/OptionsDecoder.cs
@@ -1,4 +1,5 @@
 ﻿using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
+using System.Text.RegularExpressions;
 
 namespace AoE2DELobbyBrowser.WebApi.Reliclink
 {
@@ -31,8 +32,7 @@ namespace AoE2DELobbyBrowser.WebApi.Reliclink
 
         public static Dictionary<int, string> DecodedToDict(string decoded)
         {
-            var separators = new[] { "\u0003\0\0\0", "\u0004\0\0\0", "\u0005\0\0\0", "\u0006\0\0\0", "\b\0\0\0", "\f\0\0\0", "\u001f\0\0\0", "\t\0\0\0" };
-            var splitted = decoded.Split(separators, StringSplitOptions.None);
+            var splitted = Regex.Split(decoded, @"[\x00-\x1F]+");
             return splitted.Where(x => x.Contains(':'))
                 .ToDictionary(x => int.Parse(x.Split(':')[0]), x => x.Split(':')[1]);
         }
